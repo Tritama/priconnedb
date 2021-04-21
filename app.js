6cf420c0de43899d2f7b9bb32b61bacdb1acdf2d
T_ritama <digicharat5@gmail.com>
2021-04-22 01:31:35 +0900

    rename port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
-
 const app = express();
+app.use(express.static('public'));
 
 const connection = mysql.createConnection({
   host: 'us-cdbr-east-03.cleardb.com',
@@ -18,7 +18,7 @@ connection.connect((err) => {
     }
     console.log('success');
   });
-  app.listen(3001);
+  app.listen(process.env.PORT || 5000);
 
 //ここからprogate参考
  
